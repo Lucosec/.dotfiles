6441df56226c5c245a06fd2cf96a59eceae4e201
Lucosec <lucas564lith@gmail.com>
2025-03-02 03:21:45 -0300

    Add some hyprland's dependencies.

--- dependencies.sh
+++ dependencies.sh
@@ -2,6 +2,6 @@
 
 [[ -e /usr/bin/paru ]] || printf "\nERROR: Paru not found\n" || exit 1
 
-paru -S hyprland alacritty nwg-look slurp mako emacs-wayland hyprpaper waybar openrgb grim slurp pavucontrol zen-browser-bin nautilus scrcpy gamescope wofi hyprutils eza bat fastfetch starship
+paru -S xdg-desktop-portal-hyprland hyprpolkitagent hyprland alacritty nwg-look slurp mako emacs-wayland hyprpaper waybar openrgb grim slurp pavucontrol zen-browser-bin nautilus scrcpy gamescope wofi hyprutils eza bat fastfetch starship
 
 [[ $? -eq 0 ]] && printf "\nSUCCESS!\n" && exit 1
