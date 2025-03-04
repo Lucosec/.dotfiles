e585d78022cfd0b77851ec398800232af69eefe6
Lucosec <lucas564lith@gmail.com>
2025-03-04 06:12:10 -0300

    clean up

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -12,12 +12,11 @@
 (add-to-list 'default-frame-alist '(font . "JetBrains Mono Nerd Font-14"))
 (add-to-list 'default-frame-alist '(alpha-background . 85))
 
-(require 'catppuccin-theme)
-(setq catppucin-falor 'mocha) ;
-(load-theme 'catppuccin t)
-
 (require 'org-bullets)
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+(require 'catppuccin-theme)
+(catppuccin-load-flavor 'mocha)
 
 (require 'smex)
 (smex-initialize)
@@ -29,6 +28,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(catppuccin))
+ '(custom-safe-themes
+   '("7bea8c8136b95e40a3def71cc2953e29d2553078ba1730d8262f1dccc586fbab"
+     default))
  '(package-selected-packages
    '(catppuccin-theme evil magit org-bullets ox-hugo ox-report smex
 		      sudo-edit)))
@@ -38,3 +41,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; Map Caps Lock to Ctrl
+(setq x-alt-keysym 'meta)
+(when (eq system-type 'gnu/linux)
+  (shell-command "setxkbmap -option ctrl:nocaps"))
+
